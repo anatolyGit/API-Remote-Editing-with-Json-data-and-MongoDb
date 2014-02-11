22b5a466a5be0e952089ad4c4387ddbfd0bbd4ed
Administrator <administrator@ubuntu.(none)>
2014-02-11 01:56:28 -0500

    initial resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reworks::Application.config.secret_key_base = 'aad0235050d7369c46da8629b66596966f99773f41e284f7d7b76640a259adbaa8b287c02f4e3e155b30c3217559793610d4aa787582f94a828ecc8abbb02835'
